fc0a78109234b7cd6216172fbc5f4ea35b7f2016
DeeBest <simphiwedladla8@gmail.com>
2024-04-26 12:15:09 +0200

    added event listerner to the load more images button and it can add an image when clicked

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const imageContainer = document.getElementById('images-container');
+
+const loadImagesBtn = document.getElementById('load-more-btn');
+loadImagesBtn.addEventListener('click', () => {
+  addNewImages();
+});
+
+function addNewImages() {
+  const newImg = document.createElement('img');
+  newImg.src = `https://picsum.photos/250.webp?random=${Math.floor(
+    Math.random() * 5000
+  )}`;
+  newImg.alt = 'random image';
+  imageContainer.appendChild(newImg);
+}
